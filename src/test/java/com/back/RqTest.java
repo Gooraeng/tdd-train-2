ba08d63096aee7c63b4d9f645eb1ca33a1af6013
Gooraeng <birdyoon1998@gmail.com>
2025-05-29 15:50:35 +0900

    Step 2 - B

--- src/test/java/com/back/RqTest.java
+++ src/test/java/com/back/RqTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class RqTest {
 
     @Test
-    @DisplayName("Rq.getActionName()")
+    @DisplayName("Rq.getActionName(\"수정?id=1\")")
     void t1() {
         Rq rq = new Rq("수정?id=1");
 
@@ -17,7 +17,7 @@ public class RqTest {
     }
 
     @Test
-    @DisplayName("Rq.getActionName()")
+    @DisplayName("Rq.getActionName(\"삭제?id=1\")")
     void t2() {
         Rq rq = new Rq("삭제?id=1");
 
